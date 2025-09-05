4c836d305306e10fd9edc2bf07fb362966f371b8
vikas-kds <vikas@kdsgroup.org>
2025-09-06 01:39:26 +0530

    Fixed logo path

--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
     <>
       <section className="w-full min-h-screen bg-[#12291E]">
         <div className="h-[87px] w-full bg-[#103826] flex items-center justify-center">
-          <Image alt="" src={"/logo/logo_White.png"} height={59} width={186} />
+          <Image alt="" src={"/Logo/logo_White.png"} height={59} width={186} />
         </div>
         <div className="px-6 lg:px-36 h-[calc(100vh-87px)]">
           <JoinUsContent />
