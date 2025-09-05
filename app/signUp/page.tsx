29d1a4fd776677832f4ab15355482919ffaace8e
vikasra98 <vikasranjan783@gmail.com>
2025-09-06 01:41:02 +0530

    Fixed logo path

--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const page = () => {
   return (
     <>
-      <section className="w-full min-h-screen bg-[#12291E]">
+      <section className="w-full min-h-screen bg-[#12291E] p">
         <div className="h-[87px] w-full bg-[#103826] flex items-center justify-center">
           <Image alt="" src={"/Logo/logo_White.png"} height={59} width={186} />
         </div>
